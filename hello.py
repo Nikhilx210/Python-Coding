c4148e54b8e4a430e22b22e441bee59bd54a9c92
Nikhil <nikhilrathee736@gmail.com>
2025-12-26 22:04:32 +0100

    Repository Initialization

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("I love germany")
+a=5
+b=10
+c=a+b
+print(c)
+
+
+for i in range(5):
+    print(i)
+    for j in range(3):
+        print("  ", j)
+        for k in range(2):
+            print("    ", k)
